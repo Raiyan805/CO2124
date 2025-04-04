4bfb5d560aedf2dfcbe9a9b354ee74c5e22c6b79
RandxmStranger <dpaciorkowski0@gmail.com>
2025-04-04 20:38:57 +0100

    added imports for @jsonmanagedxxxxxx

--- part1/src/main/java/com/example/part1/domain/Appointments.java
+++ part1/src/main/java/com/example/part1/domain/Appointments.java
@@ -1,5 +1,6 @@
 package com.example.part1.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
--- part1/src/main/java/com/example/part1/domain/Patient.java
+++ part1/src/main/java/com/example/part1/domain/Patient.java
@@ -1,5 +1,6 @@
 package com.example.part1.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.List;
